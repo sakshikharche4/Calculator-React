dd6d897de9836253ce840c758b71bc851c202d7e
sakshikharche4 <sakshikharche4@gmail.com>
2025-11-07 17:09:43 +0530

    Updated background image and fixed mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Section1 from "./compoents/Section1";
-import Input from "./compoents/Input";
 
 const App = () => {
   return (
-     <div className="h-screen w-screen flex justify-center items-center bg-[url('https://img.freepik.com/free-photo/back-school-decoration-with-pencils-calculator-space_23-2147662344.jpg?semt=ais_hybrid&w=740')] bg-cover bg-center">
+     <div className="h-screen w-screen flex justify-center items-center bg-cover bg-center">
+      <div
+  className="min-h-screen w-full flex justify-center items-center bg-cover bg-center"
+  style={{
+    backgroundImage: `url("https://img.freepik.com/free-photo/back-school-decoration-with-pencils-calculator-space_23-2147662344.jpg?semt=ais_hybrid&w=740")`,
+  }}
+>
       <div className="bg-black/40 p-8 rounded-2xl backdrop-blur-sm flex flex-col items-center">
      
       
-      <Section1 />
+      <Section1/>
     </div>
-    </div>
+    </div></div>
   );
 };
 
